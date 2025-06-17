a4849fb50b9eeabb3eeb383121c8525ca4609506
edvinfahlgren <edvinfahlgren@Edvins-MacBook-Air.local>
2025-06-17 12:50:48 -0700

    feat: add apologize function

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ export function greet(name: string) { return `Hello, ${name}!`; }
 export function farewell(name: string) { return `Goodbye, ${name}!`; }
 
 export function thank(name: string) { return `Thanks, ${name}!`; }
+
+export function apologize(name: string): string {
+    return `I’m sorry, ${name}.`;}
